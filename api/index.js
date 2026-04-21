1a1fe3df083020fbc17e85eda05cfc5876ae5584
Media 381-A <media381-a@Medias-Mac-Studio.local>
2026-04-21 16:36:38 +0500

    Auto-deploy update

--- api/index.js
+++ api/index.js
@@ -9,7 +9,8 @@ const fs = require('fs');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Make uploads dir if not exists. On Vercel, use /tmp
 const isVercel = process.env.VERCEL === '1';
